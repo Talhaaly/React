016f8a825ef42c22b0d8ca8b6b3ced3a28718d49
Talha Ali Akber <talhaaly103@gmail.com>
2024-02-09 22:54:17 +0600

    passwordGeneratorproject

--- pswGnProject/src/App.jsx
+++ pswGnProject/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
@@ -7,20 +7,30 @@ function App() {
   const [numAllowed, setnumAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(true)
   const [password, setPassword] = useState("")
-
+  // useRef hook 
+  const pswdRef = useRef(null)
   // Password Generator Method 
   const pswdGenerator = useCallback(() => {
     let pass = " "
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numAllowed) str += "0123456789"
-    if (charAllowed) str += "-_=+{}[]()'<>/?*&^%$#@!:|`"
-    for (let i = 1; i <= array.length; i++) {
-      let char = Mathfloor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+    if (charAllowed) str += "-_=+{}[]()'<>/?*&^%$#@!~:|`"
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass += str.charAt(char)
     }
 
     setPassword(pass)
   }, [length, numAllowed, charAllowed, setPassword])
+  const copypswdToClipboard = useCallback(() => {
+    pswdRef.current?.select();
+    pswdRef.current?.setSelectionRange(0, 30)
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+  useEffect(() => {
+    pswdGenerator()
+  }, [length, numAllowed, charAllowed, pswdGenerator])
+
 
   return (
     <>
@@ -31,8 +41,8 @@ function App() {
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input type="text" value={password}
             className='outline-none w-full py-1 px-3'
-            placeholder='password' readOnly />
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+            placeholder='password' readOnly ref={pswdRef} />
+          <button onClick={copypswdToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
             Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
@@ -42,10 +52,32 @@ function App() {
               min={6}
               max={100}
               value={length}
-              className='cursor-pointer' 
-              onChange={(e)=> {setLength(e.target.value)}}
-              />
-              <label>length:{length}</label>
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>length:{length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id='numberInput'
+              onChange={() => {
+                setnumAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id='characterInput'
+              onChange={() => {
+                setcharAllowed((prev) => !prev)
+              }}
+            />
+            <label htmlFor='characterInput'>Characters</label>
           </div>
         </div>
       </div>
